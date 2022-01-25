6ef24c2ae522038e7f7c5156ee88955954bab900
Ahmed Khalaf <pxlshpr@gmail.com>
2022-01-25 09:06:33 +0500

    update SwiftUISugar dependency version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/pxlshpr/SwiftHaptics", from: "0.1.0"),
-        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.2"),
+        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
