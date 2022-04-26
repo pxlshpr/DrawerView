02e8498277ade5bb2672310f5bdd21605f3fd196
Ahmed Khalaf <pxlshpr@gmail.com>
2022-04-26 13:52:56 +0500

    tag increment

--- Sources/DrawerView/DrawerView.swift
+++ Sources/DrawerView/DrawerView.swift
@@ -19,22 +19,18 @@ public enum DrawerViewState {
     case expanded
 }
 
+public class DrawerViewModel: ObservableObject {
+    @Published var isIgnoringHorizontalDrag: Bool = false
+    @Published var drawerSection: DrawerViewDragSection = .collapsedRegular
+    @Published var drawerProgress: Double = 0.0
+    @Published var drawerContentHeight: Double = 0.0
+    @Published var isDragging: Bool = false
+    @Published var isEnabled: Bool = true
+}
+
 public struct DrawerView<Content: View>: View {
 
-    public class ViewModel: ObservableObject {
-        @Published var isIgnoringHorizontalDrag: Bool = false
-        @Published var drawerSection: DrawerViewDragSection = .collapsedRegular
-        @Published var drawerProgress: Double = 0.0
-        @Published var drawerContentHeight: Double = 0.0
-        @Published var isDragging: Bool = false
-        @Published var isEnabled: Bool = true
-        
-        public init() {
-            
-        }
-    }
-    
-    @ObservedObject var vm: ViewModel
+    @ObservedObject var vm: DrawerViewModel
     
     var content: () -> Content
     
@@ -51,7 +47,7 @@ public struct DrawerView<Content: View>: View {
     
     var onStateChange: ((DrawerViewState) -> ())?
     
-    public init(viewModel: ViewModel,
+    public init(viewModel: DrawerViewModel,
                 isFullScreenWhenExpanded: Bool = false,
                 showHandle: Bool = true,
                 roundedCorners: Bool = true,
@@ -137,7 +133,7 @@ public struct DrawerView<Content: View>: View {
     }
 }
 
-public extension DrawerView.ViewModel {
+public extension DrawerViewModel {
     
     func dynamicValue(collapsed: CGFloat, regular: CGFloat, expanded: CGFloat) -> CGFloat {
         let lower: CGFloat
